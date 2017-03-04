5e965b9c976d4daf3d6eaff922a458b47a307ed9
Jelani Thompson <jelani.thompson@outlook.com>
2017-03-03 23:53:13 -0500

    escript

--- lib/lexicon.ex
+++ lib/lexicon.ex
@@ -1,2 +1,6 @@
 defmodule Lexicon do
+    def main(args) do
+        IO.puts("I see you're searching up definitions, nerd!");
+        IO.puts(args);
+    end
 end
--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule Lexicon.Mixfile do
     [app: :lexicon,
      version: "0.1.0",
      elixir: "~> 1.3",
+     escript: escript,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -15,6 +16,10 @@ defmodule Lexicon.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
+  end
+
+  def escript do
+    [main_module: Lexicon]
   end
 
   # Dependencies can be Hex packages:
