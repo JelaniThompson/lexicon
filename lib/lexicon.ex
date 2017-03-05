153605e15ffcda66bdd8d547ac45b2bb9116236f
Jelani Thompson <jelani.thompson@outlook.com>
2017-03-05 10:55:14 -0500

    Structure for definition searches

--- lib/lexicon.ex
+++ lib/lexicon.ex
@@ -1,6 +1,21 @@
 defmodule Lexicon do
     def main(args) do
-        IO.puts("I see you're searching up definitions, nerd!");
-        IO.puts(args);
+        args |> parse_args |> process |> cleanup
+    end
+
+    # Parse arguments into a keyword list
+    def parse_args(args) do
+        baseURL = "http://api.pearson.com/v2/dictionaries/entries"
+        options = %{ :define => "define" }
+        
+        cmd_opts = OptionParser.parse(
+            args,
+            switches: [define: :string],
+            aliases: [d: :define]
+        )
+        
+        case cmd_opts do
+           {[:define], _} -> # Search here 
+        end
     end
 end
